992dfbdd044ab62098a2c785894111ea91f3067e
cliftondavies <clifton.davies@outlook.com>
2020-03-31 22:59:32 +0100

    Update methods for Pattern module

--- pattern.rb
+++ pattern.rb
@@ -1,26 +1,26 @@
 module Pattern
-  def my_all_pattern?(pattern)
+  def self.my_all_pattern?(enum, pattern)
     result = true
 
     if pattern.is_a?(Class)
-      to_a.my_each { |item| result = false unless item.is_a?(pattern) }
+      enum.to_a.my_each { |item| result = false unless item.is_a?(pattern) }
     elsif pattern.is_a?(Regexp)
-      to_a.my_each { |item| result = false unless item.match(pattern) }
+      enum.to_a.my_each { |item| result = false unless item.match(pattern) }
     else
-      to_a.my_each { |item| result = false unless item == pattern }
+      enum.to_a.my_each { |item| result = false unless item == pattern }
     end
     result
   end
 
-  def my_any_pattern?(pattern)
+  def self.my_any_pattern?(enum, pattern)
     result = false
 
     if pattern.is_a?(Class)
-      to_a.my_each { |item| result = true if item.is_a?(pattern) }
+      enum.to_a.my_each { |item| result = true if item.is_a?(pattern) }
     elsif pattern.is_a?(Regexp)
-      to_a.my_each { |item| result = true if item.match(pattern) }
+      enum.to_a.my_each { |item| result = true if item.match(pattern) }
     else
-      to_a.my_each { |item| result = true if item == pattern }
+      enum.to_a.my_each { |item| result = true if item == pattern }
     end
     result
   end
