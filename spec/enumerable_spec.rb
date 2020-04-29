56bbc4bebd57ff0317164a5d2f387f978385c4f8
cliftondavies <clifton.davies@outlook.com>
2020-04-29 17:45:25 +0100

    Change 'or' following 'without' context, to 'nor'

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -45,7 +45,7 @@ describe Enumerable do
 
   describe '#my_all?' do
     context 'when none of the elements return false/nil' do
-      context 'without block or pattern argument' do
+      context 'without block nor pattern argument' do
         it 'returns true' do
           expect([].my_all?).to eql(true)
         end
@@ -65,7 +65,7 @@ describe Enumerable do
     end
 
     context 'when at least one of the elements return false/nil' do
-      context 'without block or pattern argument' do
+      context 'without block nor pattern argument' do
         it 'returns false' do
           expect([nil, true, 99].my_all?).not_to eql(true)
         end
@@ -87,7 +87,7 @@ describe Enumerable do
 
   describe '#my_any?' do
     context 'when at least one of the elements return true' do
-      context 'without block or pattern argument' do
+      context 'without block nor pattern argument' do
         it 'returns true' do
           expect([nil, true, 99].my_any?).to eql(true)
         end
@@ -107,7 +107,7 @@ describe Enumerable do
     end
 
     context 'when none of the elements return true' do
-      context 'without block or pattern argument' do
+      context 'without block nor pattern argument' do
         it 'returns false' do
           expect([].my_any?).not_to eql(true)
         end
@@ -129,7 +129,7 @@ describe Enumerable do
 
   describe '#my_none?' do
     context 'when none of the elements return true' do
-      context 'without block or pattern argument' do
+      context 'without block nor pattern argument' do
         it 'returns true' do
           expect([].my_none?).to eql(true)
         end
@@ -149,7 +149,7 @@ describe Enumerable do
     end
 
     context 'when at least one of the elements return true' do
-      context 'without block or pattern argument' do
+      context 'without block nor pattern argument' do
         it 'returns false' do
           expect([nil, false, true].my_none?).not_to eql(true)
         end
@@ -170,7 +170,7 @@ describe Enumerable do
   end
 
   describe '#my_count' do
-    context 'without block or argument given' do
+    context 'without block nor argument given' do
       it 'counts the number of elements in the array' do
         expect([1, 2, 4, 2].my_count).to eql(4)
       end
@@ -190,7 +190,7 @@ describe Enumerable do
   end
 
   describe '#my_map' do
-    context 'without proc argument or block given' do
+    context 'without proc argument nor block given' do
       it 'returns an Enumerator' do
         expect([1, 2, 3, 4].my_map.is_a?(Enumerator)).to eql(true)
       end
